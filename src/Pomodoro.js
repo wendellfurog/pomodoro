297f355cda1c92ad17c38ad2b4ad1b2545697ac3
wtfurog_ <84697027+wendellfurog@users.noreply.github.com>
2022-10-04 18:39:10 +0800

    added user input for duration

--- src/Pomodoro.js
+++ src/Pomodoro.js
@@ -2,12 +2,13 @@ import React, { useState, useEffect } from 'react'
 
 
 export default function Pomodoro() {
-  const [minutes, setMinutes] = useState(24);
+  const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
   const [start, setStart] = useState(false);
   const [displayMessage, setDisplayMessage] = useState(false);
 
   useEffect(() =>{
+    if(start) {
     let interval = setInterval(() => {
       clearInterval(interval);
 
@@ -26,8 +27,11 @@ export default function Pomodoro() {
       } else {
         setSeconds(seconds-1);
       }
+    
     }, 1000)
-  }, [seconds]);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [seconds, start]);
 
   const handleChange = e => {
     setMinutes(e.target.value);
@@ -35,6 +39,9 @@ export default function Pomodoro() {
 
   const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
   const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  console.log('minutes: ',minutes);
+  console.log('seconds: ',seconds);
 
   return (
     <div className='pomodoro'>
